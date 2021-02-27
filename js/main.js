21dd9dd31ed6dba581ea61e39614f5de1de1998f
ahmedskulj00 <66512187+ahmedskulj00@users.noreply.github.com>
2021-02-27 14:18:40 +0100

    Improved design, and added titles of images.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
 var slider_img = document.getElementById("images");
 var images = ["Image1.jpg", "Image2.jpg", "Image3.jpg"];
+var titleHL2 = document.getElementsByClassName("title")[0];
+var titleMF2 = document.getElementsByClassName("title")[1];
+var titleAC2 = document.getElementsByClassName("title")[2];
 var i = 0;
+
+titleHL2.style.display = "block"
+titleAC2.style.display = "none"
+titleMF2.style.display = "none"
 
 function prev() {
   if (i <= 0) i = images.length;
   i--;
+  setTitle();
   return setImg();
 }
 
 function next() {
   if (i >= images.length - 1) i = -1;
   i++;
+  setTitle()
   return setImg();
 }
 
 function setImg() {
   return slider_img.setAttribute("src", "images/" + images[i]);
+}
+
+function setTitle(){
+  if (i == 0){
+    titleHL2.style.display = "block"
+    titleAC2.style.display = "none"
+    titleMF2.style.display = "none"
+  }
+  else if (i == 1){
+    titleMF2.style.display = "block"
+    titleHL2.style.display = "none"
+    titleAC2.style.display = "none"
+  }
+  else if (i == 2){
+    titleAC2.style.display = "block"
+    titleHL2.style.display = "none"
+    titleMF2.style.display = "none"
+  }
 }
